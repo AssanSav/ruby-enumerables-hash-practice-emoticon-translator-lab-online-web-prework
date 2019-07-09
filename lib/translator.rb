bee06634fa8ba4e554990b26dda4d706232b5d87
assane savadogo <assane.savadogo81@gmail.com>
2019-07-09 03:13:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,6 +3,9 @@ YAML.load_file("./lib/emoticons.yml")
 
 def load_library(emo_path)
   emo_path = YAML.load_file("./lib/emoticons.yml")
+  emo_lib = { "get_meaning" => {},
+              "get_emoticon" => {}
+  }
 end
 
 def get_japanese_emoticon
