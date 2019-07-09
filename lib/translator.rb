e79641aa767d48a83cce2d22eeb0ab1cd83f0821
assane savadogo <assane.savadogo81@gmail.com>
2019-07-09 03:48:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ end
 
 def get_japanese_emoticon(emo_path, emo_lib)
   new_path = load_library(emo_path)
-  emoticons_japanese = new_path[]
+  emoticons_japanese = new_path["get_emoticon"]
   binding.pry
 end
 
